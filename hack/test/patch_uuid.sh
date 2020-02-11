ddf10bba11cb7ac834cb50c23221a6ecd075f9ab
zhanghj <zhanghj.lc@inspur.com>
2020-02-11 03:09:34 -0500

    get the system uuid by dmidecode cmd

--- hack/test/patch_uuid.sh
+++ hack/test/patch_uuid.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
-
 provider_prefix="ics://"
-product_uuid=$(cat /sys/class/dmi/id/product_uuid)
+system_uuid=$(dmidecode -s system-uuid)
 nodename=$(kubectl get node --no-headers | awk '{print $1}' | grep $(hostname))
 
 if [ ! -z $nodename ]; then
-    kubectl patch node $nodename -p '{"spec":{"providerID":"'$provider_prefix$product_uuid'"}}'
-    kubectl describe node $nodename | grep -i providerID
+    kubectl patch node $nodename -p '{"spec":{"providerID":"'$provider_prefix$system_uuid'"}}'
+    kubectl describe node $nodename | egrep "Name:|ProviderID:"
 else
     echo "k8s node not found"
 fi
